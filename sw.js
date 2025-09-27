1c07064f68de9df33e49a92dc0f17d6c46b70499
calibroelettrico <calibroelettrico@gmail.com>
2025-09-27 20:33:11 +0200

    Update sw.js per aggiornamento PWA

--- sw.js
+++ sw.js
@@ -1,44 +1,36 @@
-const CACHE_NAME = 'ttgo-reader-v4';
-const urlsToCache = [
+const CACHE_VERSION = 'ttgo-reader-v6';
+const CACHE_FILES = [
   '/',
-  '/index.html',
-  '/manifest.json'
+  'index.html',
+  'manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_VERSION).then((cache) => {
+      return cache.addAll(CACHE_FILES);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_VERSION) {
             return caches.delete(cacheName);
           }
         })
       );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
